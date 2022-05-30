789e41a0033cdf0623711e43119b947f2f09b5a3
ignizvitae <marcelobarbossa@hotmail.com>
2022-05-29 22:15:44 -0300

    foi corrigido um erro na linha 44 onde foi alterado o numero de-2 para -1 corrigindo um erro onde ao clicar em DEL apagavam duas casas decimais

--- App.js
+++ App.js
@@ -41,7 +41,7 @@ export default function App() {
     }
     switch(buttonPressed){
       case 'DEL':
-        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 2)))
+        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)))
         return
       case 'LIMPAR': // Limpa todo o conteúdo
         setLastNumber("") 
